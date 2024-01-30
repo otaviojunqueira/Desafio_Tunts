17d2af18bd926d535103540e006c5d0be56b83ec
otaviojunqueira <146572258+otaviojunqueira@users.noreply.github.com>
2024-01-30 18:18:05 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+# Autorizar a edição pela API do Google
+scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+credentials = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope) # type: ignore
+gc = gspread.authorize(credentials) # type: ignore
+
+# URL da planilha
+url_worksheet = "https://docs.google.com/spreadsheets/d/1H82dsamDTJp_I5jgYXVQBratIR-wknJOoWrxP_G9UIo/edit#gid=0"
+worksheets1 = gc.open_by_url(url_worksheet).sheet1
+
+# Obter todos os valores da tabela
+data = worksheets1.get_all_values()
+# Armazenar todos os valores da tabela em um DataFrame
+pandasDF = pd.DataFrame(data)
+# Delimitar a tabela
+student = pandasDF[3:30]
+
+for i in range(0, 24):
+    studentAbsences = int(student.iloc[i, 2]) # type: ignore
+    absenceCollum = i + 4
+    if studentAbsences > 15:
+        worksheets1.update_cell(absenceCollum, 7, "Reprovado por falta")
+        worksheets1.update_cell(absenceCollum, 8, "0")
+    else:
+        p1 = int(student.iloc[i, 3]) # type: ignore
+        p2 = int(student.iloc[i, 4]) # type: ignore
+        p3 = int(student.iloc[i, 5]) # type: ignore
+        avgScore = round((p1 + p2 + p3) / 30)
+        if avgScore >= 7:
+            worksheets1.update_cell(absenceCollum, 7, "Aprovado")
+            worksheets1.update_cell(absenceCollum, 8, "0")
+        elif avgScore >= 5 or avgScore < 7:
+            gradeFin = 10 - avgScore
+            worksheets1.update_cell(absenceCollum, 7, "Exame Final")
+            worksheets1.update_cell(absenceCollum, 8, gradeFin)
+        else:
+            worksheets1.update_cell(absenceCollum, 7, "Reprovado por nota")
+            final_grade = round(avgScore) + 1  
+            worksheets1.update_cell(absenceCollum, 8, final_grade)
